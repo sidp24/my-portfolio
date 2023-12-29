72f4bfc582cdf7e012ca74d8d886716ab36186f9
sidp24 <56561908+sidp24@users.noreply.github.com>
2023-12-28 20:39:29 -0500

    Remove Contact component and its styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Features from './components/Features/Features';
-import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 
 function App() {
