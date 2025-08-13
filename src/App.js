595da5775159a3805587804034da1a89b76d7ae5
Siddharth Paul <sid2paul24@gmail.com>
2025-08-12 21:25:27 -0400

    tweak

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
+// src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import ExperiencePage from './pages/ExperiencePage';
 import GalleryPage from './pages/GalleryPage';
 
-// import About from './pages/About';
-// import Contact from './pages/Contact';
-// import Services from './pages/Services';
-// import Blog from './pages/Blog';
-// import NotFound from './pages/NotFound';
-
-
-
 function App() {
   return (
     <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/my-portfolio" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/experience" element={<ExperiencePage />} />
-            <Route path="/gallery" element={<GalleryPage />} />
-
-            {/* <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} /> */}
-          </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<ExperiencePage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+      </Routes>
     </Router>
   );
 }
